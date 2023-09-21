66d46a14b7d190b3838cb38fb162796096dc941f
mlwelker <nutria.watts.0g@icloud.com>
2023-09-21 12:11:33 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Michael Welker on 2023-09-21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
