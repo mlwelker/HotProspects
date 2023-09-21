79acc02c46a2c376d99cc7d5a54b9635ebcd06a7
mlwelker <nutria.watts.0g@icloud.com>
2023-09-21 12:25:44 -0500

    environment object + basic views

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -1,13 +1,33 @@
 
 import SwiftUI
 
+@MainActor class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
 struct ContentView: View {
+    @StateObject private var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            EditView().environmentObject(user)
+            DisplayView().environmentObject(user)
         }
         .padding()
     }
