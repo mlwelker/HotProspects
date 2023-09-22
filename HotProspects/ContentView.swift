c5ec25de38819bc6ed13fb6097aa8fbc6b298844
mlwelker <nutria.watts.0g@icloud.com>
2023-09-22 00:46:23 -0500

    publishing changes with @Published

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -1,20 +1,23 @@
 
 import SwiftUI
 
+@MainActor class DelayedUpdater: ObservableObject {
+    @Published var value = 0
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
+
 struct ContentView: View {
-    @State private var selectedTab = "One"
+    @StateObject private var updater = DelayedUpdater()
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture { selectedTab = "Two" }
-                .tabItem { Label("One", systemImage: "star") }
-            Text("Tab 2")
-                .tabItem { Label("Two", systemImage: "circle") }
-                .tag("Two")
-            Text("Tab 3")
-                .tabItem { Label("Three", systemImage: "square") }
-        }
+        Text("Value is \(updater.value)")
     }
 }
 
