f0980845f45c2a6ee84510ab4e143353abe4c523
mlwelker <nutria.watts.0g@icloud.com>
2023-09-23 17:23:38 -0500

    custom swipe actions

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -2,31 +2,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
-    
     var body: some View {
-        VStack {
-            Text("hello world")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change color")
-                .padding()
-                .contextMenu {
-                    Button {
-                        backgroundColor = .red
+        List {
+            Text("Taylor Swift")
+                .swipeActions {
+                    Button(role: .destructive) {
+                        print("Deleting")
                     } label: {
-                        Label("Red", systemImage: "checkmark.circle.fill")
-                            .foregroundColor(.red) // notice that this color is ignored in the iOS context menu
+                        Label("Delete", systemImage: "minus.circle")
                     }
-                    
-                    Button("Green") {
-                        backgroundColor = .green
+                }
+                .swipeActions(edge: .leading) {
+                    Button {
+                        print("Pinning")
+                    } label: {
+                        Label("Pin", systemImage: "pin")
                     }
-                    
-                    Button("Blue") {
-                        backgroundColor = .blue
-                    }
+                    .tint(.orange)
                 }
         }
     }
