509bb76a3be8b5a597867656f7d0b53e6674818f
mlwelker <nutria.watts.0g@icloud.com>
2023-09-23 17:36:29 -0500

    local user notifications

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -1,25 +1,32 @@
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Text("Taylor Swift")
-                .swipeActions {
-                    Button(role: .destructive) {
-                        print("Deleting")
-                    } label: {
-                        Label("Delete", systemImage: "minus.circle")
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set.")
+                    } else if let error {
+                        print(error.localizedDescription)
                     }
                 }
-                .swipeActions(edge: .leading) {
-                    Button {
-                        print("Pinning")
-                    } label: {
-                        Label("Pin", systemImage: "pin")
-                    }
-                    .tint(.orange)
-                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the dogs"
+                content.subtitle = "They look hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
         }
     }
 }
