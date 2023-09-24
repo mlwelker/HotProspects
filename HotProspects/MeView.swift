c71e57344a48dcad4085709b7cc276bffb806e1e
mlwelker <nutria.watts.0g@icloud.com>
2023-09-23 22:13:14 -0500

    context menu to save QR code to photos (still blurry tho)

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -6,6 +6,7 @@ import SwiftUI
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var email = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -19,14 +20,29 @@ struct MeView: View {
                 TextField("Email", text: $email)
                     .textContentType(.emailAddress)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(email)"))
+                Image(uiImage: qrCode)
                     .resizable()
                     .interpolation(.none)
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("Your Code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name, updateCode)
+            .onChange(of: email, updateCode)
         }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(email)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
