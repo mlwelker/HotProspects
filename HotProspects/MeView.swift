d4e55c4383521ec45a2ab3afad4791b4bcd2a0a7
mlwelker <nutria.watts.0g@icloud.com>
2023-09-23 19:08:55 -0500

    tab bar

---
+++ HotProspects/MeView.swift
@@ -0,0 +1,12 @@
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text("Hello, me!")
+    }
+}
+
+#Preview {
+    MeView()
+}
---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,32 @@
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    var body: some View {
+        NavigationView {
+            Text("Hello, prospects!")
+                .navigationTitle(title)
+        }
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+}
+
+#Preview {
+    ProspectsView(filter: .none)
+}
